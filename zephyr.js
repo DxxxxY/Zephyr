31fc545c57d7fafc59bd9824accbbe6bf5bd0d24
DxxxxY <introsdream@gmail.com>
2021-12-08 20:42:17 -0500

    attributes, more tags, performance fix, font change, custom fix

--- zephyr.js
+++ zephyr.js
@@ -1,21 +1,18 @@
-// Dark mode
-var dark = true
+var dark = false
 var darkcolor = "#4a94fa"
 var lightcolor = "#990099"
 var darkbgcolor = "#000000"
 var lightbgcolor = "#ffffff"
-if (localStorage.getItem("current_theme") == "light") dark = false
-else localStorage.setItem("current_theme", dark ? "dark" : "light")
 
 var i = 0
 
-//Loop
-setInterval(() => {
-    //360deg reset
-    if (i == 360) i = 0
+const darkSwitch = () => {
+    if (localStorage.getItem("current_theme") == "dark") localStorage.setItem("current_theme", "light")
+    else if (localStorage.getItem("current_theme") == "light") localStorage.setItem("current_theme", "dark")
+    else localStorage.setItem("current_theme", dark ? "dark" : "light")
+    dark = !dark
 
     document.querySelectorAll("za").forEach(navlink => {
-        console.log(window.location.pathname.split("/").pop())
         if (window.location.pathname.split("/").pop() == "")
             if (navlink.getAttribute("href") == "index.html") return navlink.className = "active"
         if (navlink.getAttribute("href") == window.location.pathname.split("/").pop()) return navlink.className = "active"
@@ -24,43 +21,9 @@ setInterval(() => {
 
     document.querySelector("zdark").innerText = dark ? "Light Mode" : "Dark Mode"
 
-    // Apply site wide color scheme
     document.querySelector("*").style.setProperty("--color", dark ? darkcolor : lightcolor)
     document.querySelector("*").style.setProperty("--bgColor", dark ? darkbgcolor : lightbgcolor)
-
-    //Rotate gradient
-    document.querySelectorAll("zh1").forEach(e => {
-        e.style.background = dark ? `linear-gradient(${i}deg, rgba(2, 0, 36, 1) 25%, rgba(2, 0, 36, 1) 50%, #4a94fa 75%, #4a94fa 100%)` : `linear-gradient(${i}deg, rgb(117, 22, 117) 25%, rgb(82, 41, 82) 50%, rgb(75, 23, 75) 75%, rgb(82, 41, 82) 100%)`
-        e.style.webkitBackgroundClip = "text"
-    })
-    i++
-}, 10)
-
-document.addEventListener("DOMContentLoaded", () => document.querySelectorAll("zloader").forEach(loader => { if (loader.getAttribute("waitFor") == "windowLoad") loader.remove() }))
-
-HTMLElement.prototype.attributeToCSS = function() {
-    Array.prototype.slice.call(this.attributes).forEach(a => {
-        if (a.name == "href") {
-            this.addEventListener("click", (() => window.location.href = a.value))
-            return this.style.cursor = "pointer"
-        }
-        this.style[a.name] = a.value
-    })
 }
-
-document.querySelector("zdark").addEventListener("click", () => {
-    if (localStorage.getItem("current_theme") == "dark") localStorage.setItem("current_theme", "light")
-    else if (localStorage.getItem("current_theme") == "light") localStorage.setItem("current_theme", "dark")
-    dark = !dark
-})
-
-Array.prototype.slice.call(document.querySelector("zconfig").attributes).forEach(a => {
-    if (a.name.startsWith("--")) return document.querySelector("*").style.setProperty(a.name.split("--").pop(), a.value)
-    if (a.name.startsWith("$")) return window[a.name.split("$").pop()] = a.value
-    document.querySelector("*").style[a.name] = a.value
-})
-
-document.querySelectorAll("zloader, znav, zl, za, zh1, zdark, zbody").forEach(custom => custom.attributeToCSS())
 
 const shadeColor = (color, decimal) => {
     const base = color.startsWith('#') ? 1 : 0;
@@ -83,3 +46,46 @@ const shadeColor = (color, decimal) => {
 
     return `#${rr}${gg}${bb}`;
 }
+
+HTMLElement.prototype.attributeToCSS = function() {
+    Array.prototype.slice.call(this.attributes).forEach(a => {
+        if (a.name == "href") {
+            this.addEventListener("click", (() => window.location.href = a.value))
+            return this.style.cursor = "pointer"
+        }
+        this.style[a.name] = a.value
+    })
+}
+
+setInterval(() => {
+    if (i == 360) i = 0
+
+    document.querySelectorAll("zh1").forEach(e => {
+        e.style.background = dark ? `linear-gradient(${i}deg, rgba(2, 0, 36, 1) 25%, rgba(2, 0, 36, 1) 50%, #4a94fa 75%, #4a94fa 100%)` : `linear-gradient(${i}deg, rgb(117, 22, 117) 25%, rgb(82, 41, 82) 50%, rgb(75, 23, 75) 75%, rgb(82, 41, 82) 100%)`
+        e.style.webkitBackgroundClip = "text"
+    })
+    i++
+}, 10)
+
+document.querySelector("zbody").style.opacity = 0
+
+document.addEventListener("DOMContentLoaded", () => document.querySelectorAll("zloader").forEach(loader => {
+    if (loader.getAttribute("waitFor") == "windowLoad") {
+        if (loader.hasAttribute("delay")) {
+            return setTimeout(() => {
+                loader.remove()
+                document.querySelector("zbody").style.opacity = 1
+            }, loader.getAttribute("delay"))
+        }
+        loader.remove()
+        document.querySelector("zbody").style.opacity = 1
+    }
+}))
+
+document.querySelector("zdark").addEventListener("click", darkSwitch)
+
+document.querySelectorAll('*').forEach(e => { if (e.nodeName.toLowerCase().startsWith("z")) e.attributeToCSS() })
+
+Array.prototype.slice.call(document.querySelector("zconfig").attributes).forEach(a => { if (a.name.startsWith("$")) window[a.name.split("$").pop()] = a.value })
+
+darkSwitch()
